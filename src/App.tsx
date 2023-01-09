1fc3c234eb78836d7471edf75c48e2c41a6f4b33
GeniuStone <dugustlr@naver.com>
2023-01-09 18:21:02 +0900

    [Feat] zustand로 카운터 구현해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import "./App.css";
 import React from "react";
+import styled from "styled-components";
+import create from "zustand";
 
-function App() {
-  return (
-    <div>
-      <p>안녕 리액트야</p>
-      <p>안녕 쭈스탄드야</p>
-      <p>안녕 타입스크립트야</p>
-    </div>
-  );
+interface UseStore {
+  count: number;
+  plusCount: () => void;
+  minusCount: () => void;
 }
+
+const useStore = create<UseStore>((set) => ({
+  count: 0,
+  plusCount() {
+    set((state) => ({ count: state.count + 1 }));
+  },
+  minusCount() {
+    set((state) => ({ count: state.count - 1 }));
+  },
+}));
+
+const Test = () => {
+  const { count } = useStore();
+
+  return <div>카운트: {count}</div>;
+};
+
+const App = () => {
+  const { plusCount, minusCount } = useStore();
+
+  return (
+    <AppStyle>
+      <Test />
+      <button onClick={() => plusCount()}>증가</button>
+      <button onClick={() => minusCount()}>감소</button>
+    </AppStyle>
+  );
+};
+
+const AppStyle = styled.div`
+  text-align: center;
+`;
 
 export default App;
