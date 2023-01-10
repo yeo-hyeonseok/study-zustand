80addc610c8834c839f148e4318ed074a8cbd824
GeniuStone <dugustlr@naver.com>
2023-01-10 18:33:05 +0900

    [Feat] devtools 미들웨어 적용해보기

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
 interface UseStore {
   count: number;
@@ -6,12 +7,16 @@ interface UseStore {
   minusCount: () => void;
 }
 
-export const useStore = create<UseStore>((set) => ({
+const store = (set: any) => ({
   count: 0,
   plusCount() {
-    set((state) => ({ count: state.count + 1 }));
+    set((state: any) => ({ count: state.count + 1 }));
   },
   minusCount() {
-    set((state) => ({ count: state.count - 1 }));
+    set((state: any) => ({ count: state.count - 1 }));
   },
-}));
+});
+
+export const useStore = create<UseStore>()(
+  process.env.NODE_ENV !== "production" ? devtools(store) : store
+);
